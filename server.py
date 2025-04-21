b4e45f7f0ea427403f48f596f82515c5a8faebb6
lorraine-xu <yuxu@davidson.edu>
2025-04-21 16:26:46 -0400

    complete learning parts

--- server.py
+++ server.py
@@ -10,22 +10,55 @@ def home():
 
 @app.route('/china-map')
 def china_map():
-    with open('data/region_status.json', 'r') as f:
-        region_status = json.load(f)
-
     with open('data/regions.json', 'r') as f:
         regions = json.load(f)
 
     max_progress = len(regions)
-    progress = sum(1 for status in region_status.values() if status is True)
+    progress = sum(1 for region in regions.values() if region.get("status") is True)
+
+    region_images = {}
+    for region_name, data in regions.items():
+        demo_dishes = data.get("demo_dishes", [])
+        if demo_dishes:
+            first_dish = demo_dishes[0].split('(')[0].strip()
+            filename = first_dish.lower().replace("'", "").replace(" ", "_") + ".png"
+            region_images[region_name] = f"/static/media/{region_name}/{filename}"
 
     return render_template(
         'china_map.html',
-        region_status=region_status,
+        regions=regions,
+        region_images=region_images,
         progress=progress,
         max_progress=max_progress,
         hovered_region=None
     )
+
+@app.route('/learn_culture/<region>', methods=['GET'])
+def view_culture(region):
+    region_key = region.capitalize()
+
+    with open('data/regions.json', 'r') as f:
+        regions = json.load(f)
+
+    return render_template('view_culture.html', region=region_key, regions=regions)
+
+@app.route('/learn_cuisine/<region>', methods=['GET'])
+def view_cuisine(region):
+    region_key = region.capitalize()
+
+    with open('data/regions.json', 'r') as f:
+        regions = json.load(f)
+
+    return render_template('view_cuisine.html', region=region_key, regions=regions)
+
+@app.route('/quiz/<region>', methods=['GET'])
+def view_quiz(region):
+    region_key = region.capitalize()
+
+    with open('data/regions.json', 'r') as f:
+        regions = json.load(f)
+
+    return render_template('view_quiz.html', region=region_key, regions=regions)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- static/china_map.js
+++ static/china_map.js
@@ -1,49 +1,40 @@
 function setupRegionHover() {
-    const regionImages = {
-      Jiangnan: "/static/media/Jiangnan/gui_hua_tang_ou.png",
-      Xinjiang: "/static/media/Xinjiang/big_plate_chicken.png",
-      Dongbei: "/static/media/Dongbei/dumpling.png",
-      Sichuan: "/static/media/Sichuan/fried_pork_kidney.png",
-      Guangdong: "/static/media/Guangdong/char_siu.png"
-    };
-  
-    let hideTimeout;
-  
-    $(".dot").on("mouseenter", function () {
-      const $dot = $(this);
-      const region = $dot.data("region");
-  
-      clearTimeout(hideTimeout);
-  
-      // Update content in the popup
-      $("#region-image").attr("src", regionImages[region]);
-      $("#region-name").text(region);
-      $("#explore-link").attr("href", "/learn/" + region.toLowerCase());
-  
-      // Position and show popup
-      const offsetTop = $dot.position().top;
-      const offsetLeft = $dot.position().left;
-      $("#region-info")
-        .removeClass("hidden")
-        .css({
-          top: offsetTop - 90 + "px",
-          left: offsetLeft + 40 + "px"
-        });
-    });
-  
-    $(".dot").on("mouseleave", function () {
-      hideTimeout = setTimeout(() => {
-        $("#region-info").addClass("hidden");
-      }, 200);
-    });
-  
-    $("#region-info").on("mouseenter", function () {
-      clearTimeout(hideTimeout);
-    });
-  
-    $("#region-info").on("mouseleave", function () {
+  let hideTimeout;
+
+  $(".dot").on("mouseenter", function () {
+    const $dot = $(this);
+    const region = $dot.data("region");
+
+    clearTimeout(hideTimeout);
+
+    // Update content in the popup
+    $("#region-image").attr("src", regionImages[region]);
+    $("#region-name").text(region);
+    $("#explore-link").attr("href", "/learn_culture/" + region.toLowerCase());
+
+    const offsetTop = $dot.position().top;
+    const offsetLeft = $dot.position().left;
+    $("#region-info")
+      .removeClass("hidden")
+      .css({
+        top: offsetTop - 90 + "px",
+        left: offsetLeft + 40 + "px"
+      });
+  });
+
+  $(".dot").on("mouseleave", function () {
+    hideTimeout = setTimeout(() => {
       $("#region-info").addClass("hidden");
-    });
+    }, 200);
+  });
+
+  $("#region-info").on("mouseenter", function () {
+    clearTimeout(hideTimeout);
+  });
+
+  $("#region-info").on("mouseleave", function () {
+    $("#region-info").addClass("hidden");
+  });
 }
 
 $(document).ready(function () {
